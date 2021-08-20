deaa4a19f0d20a53a78718ae96c640defa7fbc88
Lizaatis1 <lizaatis@outlook.com>
2021-08-20 00:22:01 -0700

    My first resume website

---
+++ skills.bat
@@ -0,0 +1,49 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <link href="styles.css" rel="stylesheet"> 
+
+
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style.CSS">
+
+       
+        <title>Personal Portfolio</title>
+    </head>
+    <body color="white">
+        <div class="container">
+            <h1 style="color:red;font-size:45px;">SKILLS</h1>
+            <p style="color:black;font-size:20px;margin:0;"><h2>Skills</h2>
+            <h3>Technical Skills</h3>
+            <p>
+            <ul>
+                <li>
+                <span id="course-name">Operating Systems:</span>Windows Server 2012, Windows server 2008, Windows 7, Windows 10, Linux (Ubuntu and Kali)</li>
+                <li>
+                <span id="course-name">Application Software:</span office 2013, 2016, Excel sheet .</li>
+                <li>
+                <span id="course-name">Programming Skills:</span>HTML, CSS, Python, HTML, PHP, JavaScript for frontend developement</li></ul>
+
+            </p>
+            </p>
+
+            <h3>Soft Skills</h3>
+            <p>
+            <ul>
+                <li>
+                <span id="course-name">Communication and Interpersonal Skills</span></li>
+                <li>
+                <span id="course-name">Team work and cooperation</span></li>
+                <li>
+                <span id="course-name">Creativity</li>
+                 <li>
+                <span id="course-name">Time management</li>
+                </ul>
+
+            </p>
+            </p>
+    
+
+    </body>
+    </html>
